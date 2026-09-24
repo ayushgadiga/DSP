23dc00d1951aa69293b8e809533588819579ef63
ayushgadiga <gadigaayush@gmail.com>
2026-09-24 10:01:43 +0530

    Refactor LinkedList class by removing duplicates
    Removed duplicate class definition and unused methods from LinkedList class.

--- program2.py
+++ program2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Node:
     def __init__(self,data):
         self.data=data
@@ -58,65 +57,3 @@ if __name__=="__main__":
     ll.display()
 
     ll.deletion_at_end()
-=======
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-class LinkedList:
-    def __init__(self):
-        self.head=None
-
-    def insert_at_end(self,data):
-            new_node=Node(data)
-            if self.head is None:
-                self.head=new_node
-                return
-            temp=self.head
-            while temp.next:
-                temp=temp.next
-            temp.next=new_node
-
-    def deletion_at_beginning(self):
-        current=self.head
-        if self.head is None:
-            print("No data element to delete")
-        elif current==self.head:
-            self.head=current.next
-
-    def deletion_at_position(self,position):
-        if position<0:
-            print("Invalid position")
-            return
-
-        
-
-    def deletion_at_end(self):
-        current=self.head
-        while current.next.next is not None:
-            current=current.next
-        current.next=None
-
-    def display(self):
-        temp=self.head
-        elements=[]
-        while temp:
-            elements.append(str(temp.data))
-            temp=temp.next
-        print(" -> ".join(elements)if elements else "list is empty")
-
-if __name__=="__main__":
-    ll=LinkedList()
-
-    ll.insert_at_end(10)
-    ll.insert_at_end(20)
-    ll.insert_at_end(30)
-    ll.insert_at_end(40)
-    ll.display()
-
-    ll.deletion_at_beginning()
-    ll.display()
-
-    ll.deletion_at_end()
->>>>>>> 7ddf98d7b06a1a3852eedeb98cf2d445c1cbc298
-    ll.display()
